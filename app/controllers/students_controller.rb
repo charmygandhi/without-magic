d918fc1627255a112f4580484a40ce37f4146e67
Charmy Gandhi <charmygandhi93@gmail.com>
2016-10-28 12:00:34 -0500

    We now have working index and show action with associated view

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,13 @@
 class StudentsController < ApplicationController
-  def index
-  end
 
-  def show
+  def index
+  	@students = Student.order(:name) #Load all students ordered by name
   end
+  #Automatically load the app/view/students/index.html.erb	
+
+ 
+  def show
+  	@student = Student.find(params[:id]) #Load students mentioned by id in the route
+  end
+   #Automatically load the app/view/students/show.html.erb
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+	# routes
+	# HTTP verbs: get, post, put, patch, delete
+	# verb 'url/path'
   get 'students/index'
 
-  get 'students/show'
+  # SHOW Routes
+  get 'students'	=> 'students#index' #Student index (Show All Students)
+  get 'students/:id' => 'students#show', as: 'student', id: /\d+/ #Student Show (Show a Student)
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
